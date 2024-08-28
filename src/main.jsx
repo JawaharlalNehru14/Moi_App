267f3e9ad8b593acd59801a76fb21c85d95e0aa7
JawaharlalNehru14 <jnehru902@gmail.com>
2024-08-28 20:17:47 +0530

    Fix: Corrected the mistake in the code

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import {BrowserRouter} from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/app'>
+    <BrowserRouter basename='/Moi_App'>
     <App />
     </BrowserRouter>
   </React.StrictMode>,
